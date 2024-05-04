8b0e738ae009a1449fbb4f0355a5cb495cacdaf7
mwadt <mbrincko93@gmail.com>
2024-05-03 20:34:32 -0700

    added css styling to create, edit, and show pages

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+app.use(express.static('public')) 
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
